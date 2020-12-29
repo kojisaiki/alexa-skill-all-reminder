4628fe65e73f9ef25c78145df7b02b838d948ba8
Koji Saiki <saikou9901@gmail.com>
2020-12-29 13:55:39 +0900

    update

--- amzn1.ask.skill.7a4e1c6e-2ecc-42f1-8f0c-4d46513470dd/lambda/index.js
+++ amzn1.ask.skill.7a4e1c6e-2ecc-42f1-8f0c-4d46513470dd/lambda/index.js
@@ -1,12 +1,5 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
 const Alexa = require('ask-sdk-core');
-const { google } = require('googleapis');
 require('dotenv').config();
-const moment = require('moment-timezone');
 
 const func = require('./func');
 const constants = require('./constants');
